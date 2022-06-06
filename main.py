55672075fe62986ab53b37dc8863648b6f09db6c
Janis <janis.goldmanis@gmail.com>
2022-06-06 21:39:37 +0300

    Final version

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-def parseInt(input):
-    single_digit = {'one': 1, 'two': 2, 'three': 3, ' four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+def parse_int(input):
+    single_digit = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+                    'nine': 9}
 
     double_digit = {'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16,
                     'seventeen': 17, 'eighteen': 18, 'nineteen': 19, 'twenty': 20, 'thirty': 30, 'forty': 40,
                     'fifty': 50, 'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90}
     result = 0
 
-    #Searching for keywords - million, thousand or hundred, order is important
+    if len(input) == 0:
+        return 0
+
+    input = input.replace('-', ' ')
+    input = input.replace(' and', '')
 
     list = input.split(' ')
+
     if 'million' in list:
-        print(list.index('million'))
-        print(list)
+        return 10 ** 6 * parse_int(' '.join(list[0:list.index('million')])) + parse_int(
+            ' '.join(list[list.index('million') + 1:]))
 
-    #sum from dictionary everything before keyword
+    elif 'thousand' in list:
+        return 10 ** 3 * parse_int(' '.join(list[0:list.index('thousand')])) + parse_int(
+            ' '.join(list[list.index('thousand') + 1:]))
 
-    #everything before the keyword is multiplied by keyword multiplier
+    elif 'hundred' in list:
+        return 10 ** 2 * parse_int(' '.join(list[0:list.index('hundred')])) + parse_int(
+            ' '.join(list[list.index('hundred') + 1:]))
 
-    #recursive function return result + parInt everything after keyword
+    else:
+        for number in list:
+            if number in single_digit:
+                result += single_digit[number]
+            elif number in double_digit:
+                result += double_digit[number]
+    return result
 
-
-    #testingGit
-
-
-
-
-
-
-parseInt('one million three hundred')
